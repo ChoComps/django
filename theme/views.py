b83f28cba8d7e4a75401191555b848a6367a2e8f
조도형h <cdoh63@gmail.com>
2021-11-07 18:46:45 +0900

    장고

---
+++ theme/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.views.generic.base import TemplateView
+
+class MainpageView(TemplateView):
+    template_name = 'theme/main.html'
